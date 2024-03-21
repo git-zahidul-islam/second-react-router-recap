38f364f77755d6b9a28cc8c7df8bcac423372c6e
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-03-21 08:00:07 +0600

    react router recapt project start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl bg-pink-300 font-bold underline">
+        Hello world!
+      </h1>
+
+    </>
+  )
+}
+
+export default App
